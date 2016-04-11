2bc7f229238682ff28a2db9cca0fa3d8487a17f4
Eric P. Katz <ericpkatz@gmail.com>
2016-04-11 14:35:31 -0400

    add a test for update

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 - states.spec
 - customers.ctrl.spec
 - customer.ctrl.spec
+- in order to avoid massive failures comment out all tests when startin and uncomment one at a time
+
--- test/browser/customer.ctrl.spec.js
+++ test/browser/customer.ctrl.spec.js
@@ -23,7 +23,6 @@ describe('CustomersCtrl', function(){
   describe('deleting a customer', function(){
     it('goes to listing page', function(){
       var $scope = $rootScope.$new();
-      var state;
       var customer = { id: 3, name: 'Moe'};
       $controller('CustomerCtrl', {$scope: $scope, customer: customer });
       var factorySpy = spyOn(CustomerFactory, '_delete').and.callFake(function(){
@@ -34,6 +33,21 @@ describe('CustomersCtrl', function(){
       $scope.$digest();
       expect(factorySpy).toHaveBeenCalledWith($scope.customer);
       expect(stateSpy).toHaveBeenCalledWith('customers');
+    });
+  });
+
+  describe('updating a customer', function(){
+    it('goes to listing page', function(){
+      var $scope = $rootScope.$new();
+      var customer = { id: 3, name: 'Moe'};
+      $controller('CustomerCtrl', {$scope: $scope, customer: customer });
+      var factorySpy = spyOn(CustomerFactory, 'update').and.callFake(function(){
+        return $q.when({ id: 3, name: 'Moe'});
+      });
+
+      $scope.update();
+      $scope.$digest();
+      expect(factorySpy).toHaveBeenCalledWith($scope.customer);
     });
   });
 });
