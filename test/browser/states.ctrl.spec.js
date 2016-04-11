db75a86fdbe6f30124097e4721e160441459eb83
Eric P. Katz <ericpkatz@gmail.com>
2016-04-10 22:57:17 -0400

    state specs

---
+++ test/browser/states.ctrl.spec.js
@@ -0,0 +1,23 @@
+describe('states', function(){
+  var $state;
+  beforeEach(module('app'));
+  beforeEach(inject(function(_$state_){
+    $state = _$state_;
+  }));
+
+  describe('customers', function(){
+    it('has a url of /customers', function(){
+      var state = $state.get('customers');
+      expect(state.url).toEqual('/customers');
+      expect(state.controller).toEqual('CustomersCtrl');
+    });
+  });
+
+  describe('customer', function(){
+    it('has a url of /customers/:id', function(){
+      var state = $state.get('customer');
+      var url = $state.href(state, { id: 3});
+      expect(url).toEqual('#/customers/3');
+    });
+  });
+});
