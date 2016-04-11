3fa01a1e12ca870a239fcc9c591698d3c14913fc
Eric P. Katz <ericpkatz@gmail.com>
2016-04-11 14:29:29 -0400

    clean up tests

--- test/browser/customers.ctrl.spec.js
+++ test/browser/customers.ctrl.spec.js
@@ -23,7 +23,7 @@ describe('CustomersCtrl', function(){
       spyOn(CustomerFactory, 'fetchAll').and.callFake(function(){
         return $q.when([]);
       });
-      var CustomersCtrl = $controller('CustomersCtrl', {$scope: $scope});
+      $controller('CustomersCtrl', {$scope: $scope});
       $scope.$digest();
       expect($scope.customers.length).toEqual(0);
       expect($scope.message).toEqual('There are no customers');
@@ -33,17 +33,16 @@ describe('CustomersCtrl', function(){
   describe('there are customers', function(){
     it('sets the scope', function(){
       var $scope = $rootScope.$new();
-      spyOn(CustomerFactory, 'fetchAll').and.callFake(function(){
+      var fetchSpy = spyOn(CustomerFactory, 'fetchAll').and.callFake(function(){
         return $q.when([
-            {
-              name: 'Moe'
-            },
-            {},
-            {}
+            { name: 'Moe' },
+            { name: 'Larry' },
+            { name: 'Curly' },
         ]);
       });
-      var CustomersCtrl = $controller('CustomersCtrl', {$scope: $scope});
+      $controller('CustomersCtrl', {$scope: $scope});
       $scope.$digest();
+      expect(fetchSpy).toHaveBeenCalled();
       expect($scope.customers.length).toEqual(3);
       expect($scope.customers[0].name).toEqual('Moe');
       expect($scope.message).toBeFalsy();
@@ -53,64 +52,48 @@ describe('CustomersCtrl', function(){
   describe('inserting a customer', function(){
     it('calls the insert factory method and goes to detail page', function(){
       var $scope = $rootScope.$new();
-      var stateArguments;
 
-      spyOn(CustomerFactory, 'fetchAll').and.callFake(function(){
-        return $q.when([
-            {
-              name: 'Moe'
-            },
-            {},
-            {}
-        ]);
+      var fetchSpy = spyOn(CustomerFactory, 'fetchAll').and.callFake(function(){
+        return $q.when([]);
       });
 
-      var spy = spyOn(CustomerFactory, 'create').and.callFake(function(){
+      var createSpy = spyOn(CustomerFactory, 'create').and.callFake(function(){
         return $q.when({ id: 5, name: 'Shep'});
       });
 
       var stateSpy = spyOn($state, 'go');
 
 
-      $scope.customer = { name: 'Shep'};
-      var CustomersCtrl = $controller('CustomersCtrl', {$scope: $scope});
-      $scope.insert({name: 'shep'});
+      $scope.newCustomer = { name: 'Shep'};
+      $controller('CustomersCtrl', {$scope: $scope});
+      $scope.insert();
       $scope.$digest();
-      expect(spy).toHaveBeenCalledWith($scope.customer);
+      expect(fetchSpy).toHaveBeenCalled();
+      expect(createSpy).toHaveBeenCalledWith($scope.newCustomer);
       expect(stateSpy).toHaveBeenCalledWith('customer', {id: 5});
-
     });
   });
 
   describe('deleting a customer', function(){
-    it('calls the insert factory method and goes to detail page', function(){
+    it('calls the delete factory method and reloads data', function(){
       var $scope = $rootScope.$new();
-      var stateArguments;
 
       var fetchSpy = spyOn(CustomerFactory, 'fetchAll').and.callFake(function(){
         return $q.when([
-            {
-              name: 'Moe'
-            },
-            {},
-            {}
+            { name: 'Moe' }
         ]);
       });
 
-      var spy = spyOn(CustomerFactory, '_delete').and.callFake(function(){
+      var deleteSpy = spyOn(CustomerFactory, '_delete').and.callFake(function(){
         return $q.when('');
       });
 
-      $state.go = function(){
-        stateArguments = arguments;
-      };
 
-      $scope.customer = { name: 'Shep'};
       $controller('CustomersCtrl', {$scope: $scope});
       $scope.$digest();
       $scope._delete($scope.customers[0]);
       $scope.$digest();
-      expect(spy).toHaveBeenCalledWith($scope.customers[0]);
+      expect(deleteSpy).toHaveBeenCalledWith($scope.customers[0]);
       expect(fetchSpy.calls.count()).toEqual(2);
     });
   });
